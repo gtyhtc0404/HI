4160af0773f3b80cce174888331b4dfb8f35abf2
gtyhtc0404 <102957280+gtyhtc0404@users.noreply.github.com>
2024-03-11 18:49:23 +0800

    Update bodyindex.py

--- bodyindex.py
+++ bodyindex.py
@@ -1,6 +1,6 @@
-h=float(input('請輸入身高(公分)'))/100
-w=float(input('請輸入體重(公斤)'))
+h = float(input('請輸入身高(公分)'))/100
+w = float(input('請輸入體重(公斤)'))
 print('您輸入的身高為：', h*100, '公分')
 print('您輸入的體重為：', w, '公斤')
-bmi=round(w/(h*h),2)
+bmi = round(w/(h*h),2)
 print('您的BMI數值為：', bmi)
